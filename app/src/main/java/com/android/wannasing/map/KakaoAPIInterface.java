bcc7c4823810b189421ac026eafe6f71130dfb79
Seongchan Joe <hey.jonathan.cho@gmail.com>
2021-12-02 02:31:22 +0900

    Not complete map.

---
+++ app/src/main/java/com/android/wannasing/map/KakaoAPIInterface.java
@@ -0,0 +1,19 @@
+package com.android.wannasing.map;
+
+import com.android.wannasing.map.model.ResultSearchKeyword;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+public interface KakaoAPIInterface {
+
+  @GET("v2/local/search/keyword.json")
+  Call<ResultSearchKeyword> getSearchKeyword(
+      @Header("Authorization") String key,
+      @Query("query") String query,
+      @Query("x") String x,
+      @Query("y") String y,
+      @Query("radius") Integer radius
+  );
+}
---
+++ app/src/main/java/com/android/wannasing/map/activity/permission/PermissionChecker.java
@@ -0,0 +1,38 @@
+package com.android.wannasing.map.activity.permission;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+import java.util.List;
+
+public class PermissionChecker {
+
+  PermissionListener permissionlistener;
+
+  private PermissionChecker(Runnable runnableWhenGranted, Runnable runnableWhenDenied) {
+    permissionlistener = new PermissionListener() {
+      @Override
+      public void onPermissionGranted() {
+        runnableWhenGranted.run();
+      }
+
+      @Override
+      public void onPermissionDenied(List<String> deniedPermissions) {
+        runnableWhenDenied.run();
+      }
+    };
+  }
+
+  public static PermissionChecker create(Runnable runnableWhenGranted,
+      Runnable runnableWhenDenied) {
+    return new PermissionChecker(runnableWhenGranted, runnableWhenDenied);
+  }
+
+  public void requestPermissions(String... permissions) {
+    TedPermission.create()
+        .setPermissionListener(permissionlistener)
+        .setDeniedMessage(
+            "If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+        .setPermissions(permissions)
+        .check();
+  }
+}
